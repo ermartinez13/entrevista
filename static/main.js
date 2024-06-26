b6cd2a44dc94863fe8882a73bd8c4343bb456e8d
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-26 16:11:59 +0000

    display video preview

--- static/main.js
+++ static/main.js
@@ -1,1 +1,8 @@
-console.log('Hello from main.js!')
+const startButton = document.getElementById('startPreview');
+const videoPreview = document.getElementById('preview');
+
+startButton.addEventListener('click', () => {
+  navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
+    videoPreview.srcObject = stream;
+  });
+})
